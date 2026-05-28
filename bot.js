319e0484845ead125bfb33635481cdcab2bcf519
Thiarles Dias <thiarlesdias@toptecdigital.com>
2026-05-28 02:41:06 -0300

    fix whatsapp integration

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 
 wppconnect.create({
+
   session: 'bot',
+
+  autoClose: 0,
+
   headless: true,
 
   puppeteerOptions: {
@@ -10,33 +14,47 @@ wppconnect.create({
 
 })
 .then((client) => {
+
   console.log('BOT ONLINE');
 
   client.onMessage(async (message) => {
 
-    console.log('Mensagem:', message.body);
+    console.log('Mensagem recebida:', message.body);
 
     try {
 
-      const response = await fetch('http://127.0.0.1:5000/webhook', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          numero: message.from,
-          mensagem: message.body
-        })
-      });
+      const response = await fetch(
+        'http://host.docker.internal:5000/webhook',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            numero: message.from,
+            mensagem: message.body
+          })
+        }
+      );
 
       const data = await response.json();
 
+      console.log(data);
+
       if (data.resposta) {
-        await client.sendText(message.from, data.resposta);
+
+        await client.sendText(
+          message.from,
+          data.resposta
+        );
+
       }
 
     } catch (err) {
+
+      console.log('ERRO API:');
       console.log(err);
+
     }
 
   });
