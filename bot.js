51b3d938bb10f8cdb82cedfad8abac151ad2968c
Thiarles Dias <thiarlesdias@toptecdigital.com>
2026-05-28 12:39:34 -0300

    bot funcional

---
+++ bot.js
@@ -0,0 +1,75 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+console.log('INICIANDO BOT...');
+
+const client = new Client({
+
+    authStrategy: new LocalAuth(),
+
+    webVersionCache: {
+        type: 'none'
+    },
+
+    puppeteer: {
+
+        headless: false,
+
+        executablePath:
+        'C:/Program Files/Google/Chrome/Application/chrome.exe',
+
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    }
+});
+
+client.on('qr', qr => {
+
+    console.log('QR RECEBIDO');
+
+    qrcode.generate(qr, {
+        small: true
+    });
+
+});
+
+client.on('authenticated', () => {
+
+    console.log('AUTENTICADO');
+
+});
+
+client.on('ready', () => {
+
+    console.log('BOT PRONTO');
+
+});
+
+client.on('message', async msg => {
+
+    console.log('MENSAGEM RECEBIDA:', msg.body);
+
+    if (msg.body.toLowerCase().includes('oi')) {
+
+        console.log('ENTROU NO IF');
+
+        try {
+
+            await msg.reply('Olá 😎');
+
+            console.log('RESPOSTA ENVIADA');
+
+        } catch (erro) {
+
+            console.log('ERRO AO RESPONDER');
+            console.log(erro);
+
+        }
+
+    }
+
+});
+
+client.initialize();
