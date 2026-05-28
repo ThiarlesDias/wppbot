e272eda628d472da37a9d2efb0e91ce27da76cda
Thiarles Dias <thiarlesdias@toptecdigital.com>
2026-05-28 00:41:58 -0300

    integracao flask

--- bot.js
+++ bot.js
@@ -1,27 +1,47 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 
 wppconnect.create({
-    session: 'bot'
-})
+  session: 'bot',
+  headless: true,
 
+  puppeteerOptions: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+
+})
 .then((client) => {
+  console.log('BOT ONLINE');
 
-    client.onMessage((message) => {
+  client.onMessage(async (message) => {
 
-        console.log(message.from);
-        console.log(message.body);
+    console.log('Mensagem:', message.body);
 
-        if (message.body.toLowerCase() === 'oi') {
+    try {
 
-            client.sendText(
-                message.from,
-                'Olá 😎'
-            );
-        }
-    });
+      const response = await fetch('http://127.0.0.1:5000/webhook', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          numero: message.from,
+          mensagem: message.body
+        })
+      });
+
+      const data = await response.json();
+
+      if (data.resposta) {
+        await client.sendText(message.from, data.resposta);
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+
+  });
 
 })
-
 .catch((error) => {
-    console.log(error);
+  console.log(error);
 });
